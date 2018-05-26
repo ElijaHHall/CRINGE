2f4db40d1976f07078bf86779ea523bbc770e652
William Jefferies <willjefferies10@gmail.com>
2018-05-26 15:27:37 -0600

    Made message and contact models, hooked them up to abstract user and added materialize cdn

--- CRINGE/settings.py
+++ CRINGE/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'phone_field',
+    'main_app',
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phone_field import PhoneField
 
-
 class CustomUser(AbstractUser):
 	# First/last name is not a global-friendly pattern
 	name = models.CharField(blank=True, max_length=255)
@@ -12,3 +11,5 @@ class CustomUser(AbstractUser):
 
 	def __str__(self):
 		return self.email
+
+
