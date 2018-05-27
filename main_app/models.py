3d69b987e4a59953ff7c3bdd1f61a9367bf96f2c
William Jefferies <willjefferies10@gmail.com>
2018-05-27 14:05:29 -0600

    okay

--- main_app/models.py
+++ main_app/models.py
@@ -1,17 +1,17 @@
-from django.db import models
-from phone_field import PhoneField
-from CRINGE import settings
+# from django.db import models
+# from phone_field import PhoneField
+# from CRINGE import settings
 
-class Message(models.Model):
-	body = models.CharField(max_length=250)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+# class Message(models.Model):
+# 	body = models.CharField(max_length=250)
+# 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.body
+# 	def __str__(self):
+# 		return self.body
 
-class Contact(models.Model):
-	name = models.CharField(max_length=100)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+# class Contact(models.Model):
+# 	name = models.CharField(max_length=100)
+# 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.name
+# 	def __str__(self):
+# 		return self.name
