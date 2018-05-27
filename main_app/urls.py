449766ad3c5b18b83da25541ba0661f68eaafae4
William Jefferies <willjefferies10@gmail.com>
2018-05-27 13:42:11 -0600

    pushing new branch

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [
+	path('messages/',views.messages, name='messages')
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Message, Contact
+from django.contrib.auth.models import AbstractUser
+
+def messages(request):
+    messages = Message.objects.all()
+    return render(request, 'messages.html', {'messages': messages})
+
