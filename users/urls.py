7e661890ba9ebef188e3b76d2a2b359466a8f57b
William Jefferies <willjefferies10@gmail.com>
2018-05-28 14:36:34 -0600

    fuck it

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 	path('signup/', views.SignUp.as_view(), name='signup'),
 	path('home/', views.home, name='home'),
 	path('logout/', views.logout_view, name='logout'),
-	path('post_url/', views.post_contact, name='post_contact')
+	path('home/post_contact/', views.post_contact, name='post_contact')
 ]
